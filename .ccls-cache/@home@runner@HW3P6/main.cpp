2be572c6d9f3af04cf517d30f43a0c3d8a9c7734
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-12 08:31:30 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW3P6/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+  int H;
+  int M;
+
+  cout << "What are the hour and minute values? " << endl;
+  cin >> H >> M;
+
+  
+
+  
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+  int H;
+  int M;
+
+  cout << "What are the hour and minute values? " << endl;
+  cin >> H >> M;
+
+  if ((M>59) || (M<0)) cout << "error";
+  else if (m/60 == 0)
+
+  
+}
